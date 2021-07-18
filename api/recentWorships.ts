b1b4d5c7d1b15e72f4caad46f13fe4a160ae6b3d
Cat <cat@coins.builders>
2021-07-18 11:41:27 -0700

    3333

--- api/recentWorships.ts
+++ api/recentWorships.ts
@@ -1,9 +1,9 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-import { getMeritList } from "../utils/temple";
+import { getRecentWorships } from "../utils/temple";
 
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
   try {
-    const data = await getMeritList();
+    const data = await getRecentWorships();
     res.status(200).send(data);
   } catch (error) {
     res.status(500).send({ error: "internal server error" });
--- utils/temple.ts
+++ utils/temple.ts
@@ -10,7 +10,7 @@ export const getMeritList = async (): Promise<any[]> => {
     const templeContract = getContract(templeABI, TEMPLE_CONTRACT);
     let block = await getLatestBlock();
 
-    const batch: Promise<any>[] = [];
+    const batch: Promise<any[]>[] = [];
 
     while (block > START_BLOCK) {
         batch.push(templeContract.getPastEvents("Worship", {                               
@@ -21,9 +21,35 @@ export const getMeritList = async (): Promise<any[]> => {
     }
 
     const res = await Promise.all(batch);
-    return res.sort((a,b) => b.returnValues.amount-a.returnValues.amount).slice(0,20).map(ev => { return {
+    return res.reduce((a,c)=>{
+        return a.concat(c).sort((x,y) => y.returnValues.amount-x.returnValues.amount).slice(0,20);
+    }).map(ev => { return {
         ...ev.returnValues, 
         block: ev.blockNumber,
         tx: ev.transactionHash
     }})
+};
+
+export const getRecentWorships = async (): Promise<any[]> => {
+    const templeContract = getContract(templeABI, TEMPLE_CONTRACT);
+    let block = await getLatestBlock();
+
+    let worships: any[] = [];
+
+    /* eslint-disable no-await-in-loop */
+    while (worships.length < 20 && block > START_BLOCK) {
+        const res: any[] = await templeContract.getPastEvents("Worship", {                               
+            fromBlock: block-BLOCK_RANGE, 
+            toBlock: block    
+        });
+        worships = worships.concat(res.map(ev => { return {
+            ...ev.returnValues, 
+            block: ev.blockNumber,
+            tx: ev.transactionHash
+        }}).reverse());
+        block -= BLOCK_RANGE;
+    }
+    /* eslint-enable no-await-in-loop */
+
+    return worships;
 };
