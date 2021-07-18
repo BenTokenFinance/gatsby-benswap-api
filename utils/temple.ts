78ad35d228d5cafbb6f7441fd04d4c3ad727c51d
Cat <cat@coins.builders>
2021-07-18 11:50:21 -0700

    3333

--- utils/temple.ts
+++ utils/temple.ts
@@ -5,6 +5,7 @@ import templeABI from "./abis/temple.json";
 
 const START_BLOCK = 9188546;
 const BLOCK_RANGE = 5000;
+const BLOCKS_IN_A_WEEK = 201600;
 
 export const getMeritList = async (): Promise<any[]> => {
     const templeContract = getContract(templeABI, TEMPLE_CONTRACT);
@@ -33,23 +34,24 @@ export const getMeritList = async (): Promise<any[]> => {
 export const getRecentWorships = async (): Promise<any[]> => {
     const templeContract = getContract(templeABI, TEMPLE_CONTRACT);
     let block = await getLatestBlock();
+    const earliestBlock = Math.max(START_BLOCK, block-BLOCKS_IN_A_WEEK);
 
-    let worships: any[] = [];
+    const batch: Promise<any[]>[] = [];
 
-    /* eslint-disable no-await-in-loop */
-    while (worships.length < 20 && block > START_BLOCK) {
-        const res: any[] = await templeContract.getPastEvents("Worship", {                               
+    while (block > earliestBlock) {
+        batch.push(templeContract.getPastEvents("Worship", {                               
             fromBlock: block-BLOCK_RANGE, 
-            toBlock: block    
-        });
-        worships = worships.concat(res.map(ev => { return {
-            ...ev.returnValues, 
-            block: ev.blockNumber,
-            tx: ev.transactionHash
-        }}).reverse());
+            toBlock: block
+        }))
         block -= BLOCK_RANGE;
     }
-    /* eslint-enable no-await-in-loop */
 
-    return worships;
+    const res = await Promise.all(batch);
+    return res.reduce((a,c)=>{
+        return a.concat(c).sort((x,y) => y.returnValues.amount-x.returnValues.amount).slice(0,20);
+    }).map(ev => { return {
+        ...ev.returnValues, 
+        block: ev.blockNumber,
+        tx: ev.transactionHash
+    }})
 };
